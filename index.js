20fa96583c00a01d4b67dda28397b11677305970
Kristýna Nováková <novakovkri@gmail.com>
2023-05-30 19:16:49 +0200

    Adding Dark Mode

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const toggleSwitch = document.querySelector(
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
   }
 };
 
